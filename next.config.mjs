a53af3fc9837d4009c8a236bb4a600b5841ab3ac
VictorH74 <victorh.almeida7@gmail.com>
2024-08-19 21:26:04 -0300

    build: added nextjs configs and firebase packages

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,38 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "picsum.photos",
-                port: "",
-                pathname: "/**",
-            },
-        ]
-    }
+          {
+            protocol: "https",
+            hostname: "imgs.search.brave.com",
+            port: "",
+            pathname: "/**",
+          },
+          {
+            protocol: "https",
+            hostname: "img.icons8.com",
+            port: "",
+            pathname: "/**",
+          },
+          {
+            protocol: "https",
+            hostname: "picsum.photos",
+            port: "",
+            pathname: "/**",
+          },
+          {
+            protocol: "https",
+            hostname: "fastly.picsum.photos",
+            port: "",
+            pathname: "/**",
+          },
+          {
+            protocol: "https",
+            hostname: "firebasestorage.googleapis.com",
+            port: "",
+            pathname: "/**",
+          },
+        ],
+      },
 };
 
 export default nextConfig;
